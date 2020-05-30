1a89d533e4473aba7fe226a2f47ee5be76fda785
Bazyli Polednia <bazyli.polednia@gmail.com>
2020-05-30 11:31:01 +0200

    Add corrected Q-Learning environment

--- wumpus_world/environment.py
+++ wumpus_world/environment.py
@@ -2,25 +2,38 @@ from enum import Enum
 
 
 BOUNCE_PUNISHMENT = -10
+BOARD_ID = 4
 
 class Action(Enum):
     LEFT = 0
     RIGHT = 1
     UP = 2
     DOWN = 3
-"""
-reward_array = [[-1, -1, -1, -1],
+
+reward_array1 = [[-1, -1, -1, -1],
                 [-1000, 1000, -1000, -1],
                 [-1, -1, -1, -1],
                 [-1, -1, -1000, -1]]
-"""
-reward_array = [[-1, -1000, -1, -1000],
+
+reward_array2 = [[-1, -1000, -1, -1000],
                 [-1, -1, -1000, 1000],
                 [-1, -1, -1, -1],
                 [-1, -1, -1000, -1]]
 
-def step(x, y, action):
+reward_array3 = [[-1, -1000, -1, -1],
+                [-1, -1, -1, -1],
+                [-1000, -1, -1, -1000],
+                [-1, 1000, -1, -1]]
 
+reward_array4 = [[-1, -1000, -1, -1],
+                [-1, -1, -1000, -1],
+                [-1000, -1, -1, -1000],
+                [-1, -1000, -1, 1000]]
+
+reward_array_dict = {1: reward_array1, 2: reward_array2, 3: reward_array3, 4: reward_array4}
+
+def step(x, y, action):
+    reward_array = reward_array_dict.get(BOARD_ID)
     next_x, next_y = x, y
     if action == Action.LEFT.value:
         next_x -= 1
--- wumpus_world/main.py
+++ wumpus_world/main.py
@@ -2,7 +2,6 @@ import time
 
 import numpy as np
 import random
-import matplotlib.pyplot as plt
 from wumpus_world.environment import Action, step
 
 
@@ -10,66 +9,69 @@ def get_action_with_max_value(Q, x, y):
     return Q.argmax(axis=0)[x][y]
 
 def qlearning():
+    print("Started learning...")
     # Setting learning parameters
     number_of_episodes = 20000
-    gamma = 1
-    alpha = 0.8
-    epsilon = 0.1
-    max_steps_number = 100
+    gamma = 0.9
+    alpha = 0.4
+    epsilon = 0.4
+    max_steps_number = 40
 
     num_actions = len([action.value for action in Action])
     Q = np.zeros((num_actions, 4, 4))
     sum_of_rewards = np.zeros(number_of_episodes)
 
     for episode in range(number_of_episodes):
+        if epsilon > 0.1:
+            epsilon -= 0.005
         x, y = 0, 0
         step_number = 0
         game_ended = False
         sum_of_rewards[episode] = 0
+        last_2_actions = [(None, None), (x, y)]
         while not game_ended:
             step_number += 1
-            if random.uniform(0, 1) < epsilon:
+            if random.uniform(0, 1) < epsilon: # epsilon-greedy policy
                 action = random.randrange(num_actions) # get random action
             else:
                 action = get_action_with_max_value(Q, x, y) # get action with highest value in Q for x, y
-            #print(f"Step number: {step_number}, x: {x}, y: {y}, action {action}")
-            next_x, next_y, reward, end = step(x, y, action)
-            Q[action][x][y] = Q[action][x][y] + \
-                              alpha*(reward + gamma*get_action_with_max_value(Q, next_x, next_y) - Q[action][x][y])
+            next_x, next_y, reward, end = step(x, y, action) # get next step from environment
+            if next_x == last_2_actions[0][0] and next_y == last_2_actions[0][1]: # check if agent is oscillating between 2 fields
+                reward -= 20
+            Q[action][x][y] += alpha*(reward + gamma*get_action_with_max_value(Q, next_x, next_y) - Q[action][x][y]) # TD equation
+            last_2_actions[0] = last_2_actions[1] # update environment variables
+            last_2_actions[1] = (next_x, next_y)
             x, y = next_x, next_y
             sum_of_rewards[episode] += reward
 
-            if step_number == max_steps_number or end:
+            if step_number == max_steps_number or end: # check if episode ended
                 game_ended = True
         if episode % 1000 == 0:
             print(f"Sum of rewards for episode {episode}: {sum_of_rewards[episode]}")
 
     return Q
 
-def validate(Q):
-    validation_episodes = 1000
+def run(Q):
     max_steps_number = 20
-    sum_of_rewards = np.zeros(validation_episodes)
-    #print(Q)
-    for episode in range(validation_episodes):
-        x, y = 0, 0
-        step_number = 0
-        game_ended = False
-        sum_of_rewards[episode] = 0
-        while not game_ended:
-            step_number += 1
-            action = get_action_with_max_value(Q, x, y)
-            #print(f"x: {x}, y: {y}, action: {action}")
-            next_x, next_y, reward, end = step(x, y, action)
+    x, y = 0, 0
+    step_number = 0
+    game_ended = False
+    sum_of_rewards = 0
 
-            x, y = next_x, next_y
-            sum_of_rewards[episode] += reward
+    while not game_ended:
+        step_number += 1
+        action = get_action_with_max_value(Q, x, y)
+        print(f"x: {x}, y: {y}, action: {Action(action).name}")
+        next_x, next_y, reward, end = step(x, y, action)
 
-            if step_number == max_steps_number or end:
-                game_ended = True
-        #time.sleep(10)
-    print(f"Average validation return {np.average(sum_of_rewards)}")
+        x, y = next_x, next_y
+        sum_of_rewards += reward
+
+        if step_number == max_steps_number or end:
+            game_ended = True
+
+    print(f"Score: {sum_of_rewards}")
 
 if __name__ == "__main__":
     Q = qlearning()
-    validate(Q)
+    run(Q)
