b886371fb6dcf551fccaf7b2d1836e1679804f23
Bazyli Polednia <bazyli.polednia@gmail.com>
2020-05-30 09:02:41 +0200

    Reformat code

--- wumpus_world/environment.py
+++ wumpus_world/environment.py
@@ -1,6 +1,8 @@
 from enum import Enum
 
 
+BOUNCE_PUNISHMENT = -10
+
 class Action(Enum):
     LEFT = 0
     RIGHT = 1
@@ -18,37 +20,35 @@ reward_array = [[-1, -1000, -1, -1000],
                 [-1, -1, -1000, -1]]
 
 def step(x, y, action):
-    def validate_out_of_bounds(x, y):
-        bounce_punishment = 0
-        if x > 3:
-            x = 3
-            bounce_punishment = -10
-        elif x < 0:
-            x = 0
-            bounce_punishment = -10
-        if y > 3:
-            y = 3
-            bounce_punishment = -10
-        elif y < 0:
-            y = 0
-            bounce_punishment = -10
-        return x, y, bounce_punishment
 
-    next_x, next_y = 0, 0
+    next_x, next_y = x, y
     if action == Action.LEFT.value:
-        next_x = x - 1
-        next_y = y
+        next_x -= 1
     elif action == Action.RIGHT.value:
-        next_x = x + 1
-        next_y = y
+        next_x += 1
     elif action == Action.UP.value:
-        next_x = x
-        next_y = y + 1
+        next_y += 1
     elif action == Action.DOWN.value:
-        next_x = x
-        next_y = y - 1
+        next_y -= 1
 
     next_x, next_y, bounce_punishment = validate_out_of_bounds(next_x, next_y)
     reward = reward_array[next_x][next_y] + bounce_punishment
     game_ended = reward == -1000 or reward == 1000
     return next_x, next_y, reward, game_ended
+
+def validate_out_of_bounds(x, y):
+    bounce = False
+    if x > 3:
+        x = 3
+        bounce = True
+    elif x < 0:
+        x = 0
+        bounce = True
+    if y > 3:
+        y = 3
+        bounce = True
+    elif y < 0:
+        y = 0
+        bounce = True
+    bounce_punishment = BOUNCE_PUNISHMENT if bounce else 0
+    return x, y, bounce_punishment
